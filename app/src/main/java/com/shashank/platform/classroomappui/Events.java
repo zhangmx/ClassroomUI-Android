92cb8412c1f49f4598fbb930572592f9da12d3d3
mingxing <icediv@163.com>
2022-08-02 08:48:24 +0800

    update gradle to 7.4

--- app/src/main/java/com/shashank/platform/classroomappui/Events.java
+++ app/src/main/java/com/shashank/platform/classroomappui/Events.java
@@ -1,6 +1,6 @@
 package com.shashank.platform.classroomappui;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Events extends AppCompatActivity {
--- app/src/main/java/com/shashank/platform/classroomappui/SplashScreen.java
+++ app/src/main/java/com/shashank/platform/classroomappui/SplashScreen.java
@@ -1,7 +1,7 @@
 package com.shashank.platform.classroomappui;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
